f688d799286acf0f92350bf6d48ce3b873d9f0e7
Eryk <eryk.witkowski98@gmail.com>
2025-12-15 20:07:56 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue';
+
+  import hoodieImage from './assets/images/Hoodie_1.png'
+
+  const product = ref('Hoodie');
+  const description = ref('A good looking hoodie for winter time 😎.')
+</script>
+
+<template>
+<div class="nav-bar"></div>
+<div class="product-display">
+  <div class="product-container">
+    <div class="product_image">
+      <img v-bind:src="hoodieImage">
+    </div>
+    <div class="product-info">
+      <h1>{{ product }}</h1>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</div>
+</template>
+
