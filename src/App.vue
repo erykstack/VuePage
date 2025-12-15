b7ede107c9f16a72d1a57af4a0df19394b593935
Eryk <eryk.witkowski98@gmail.com>
2025-12-15 20:32:16 +0100

    Lesson 4 commit to main

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 
   const product = ref('Hoodie');
   const description = ref('A good looking hoodie for winter time 😎.')
+  const inventory = ref(1);
+  const onSale = ref(true);
 </script>
 
 <template>
@@ -16,7 +18,10 @@
     </div>
     <div class="product-info">
       <h1>{{ product }}</h1>
-      <p>{{ description }}</p>
+      <p v-if="inventory > 10">In stock</p>
+      <p v-else-if="inventory <= 10 && inventory > 0">Almost out of stock</p>
+      <p v-else ="inventory = 0">Out of stock</p>
+      <p v-if="onSale">On Sale!</p>
     </div>
   </div>
 </div>
